be29ba8278c3cd270ed527a3ac0ec2e51ed89291
atksh <31435204+atksh@users.noreply.github.com>
2022-10-13 13:19:09 +0900

    fix

--- src/onnxmodel_utils/model.py
+++ src/onnxmodel_utils/model.py
@@ -1399,6 +1399,9 @@ class Model(Base):
                 i1.to(DType.FLOAT)
                 o1.to(DType.FLOAT)
                 graph = self.get_graph_by_node(node)
+                graph.add_tensor(i0)
+                graph.add_tensor(i1)
+                graph.add_tensor(o1)
                 n = self.make_node(
                     name=node.name + "_float",
                     op_type="MatMul",
@@ -1406,10 +1409,10 @@ class Model(Base):
                     outputs=[o1.name],
                     attrs=node.attrs,
                 )
-                graph.add_tensor(i0, recursive=True)
-                graph.add_tensor(i1, recursive=True)
-                graph.add_tensor(o1, recursive=True)
                 graph.add_node(n)
+                assert i0.name in graph.name_to_tensor
+                assert i1.name in graph.name_to_tensor
+                assert o1.name in graph.name_to_tensor
                 m = self.create_model_from_nodes(n)
                 m.infer_shapes()
                 o1.shape = m.name_to_tensor[o1.name].shape
@@ -1433,6 +1436,9 @@ class Model(Base):
                 i1.to(DType.FLOAT)
                 o1.to(DType.FLOAT)
                 graph = self.get_graph_by_node(node)
+                graph.add_tensor(i0)
+                graph.add_tensor(i1)
+                graph.add_tensor(o1)
                 n = self.make_node(
                     name=node.name + "_float",
                     op_type="Conv",
@@ -1441,9 +1447,6 @@ class Model(Base):
                     attrs=node.attrs,
                 )
                 graph.add_node(n)
-                graph.add_tensor(i0, recursive=True)
-                graph.add_tensor(i1, recursive=True)
-                graph.add_tensor(o1, recursive=True)
                 m = self.create_model_from_nodes(n)
                 m.infer_shapes()
                 o1.shape = m.name_to_tensor[o1.name].shape
